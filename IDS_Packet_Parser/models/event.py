2f6d16a4d5281fa6821193db488170872dfa5679
sup3rshy <133123465+sup3rshy@users.noreply.github.com>
2026-09-22 01:03:35 +0000

    Update models/event.py skeleton script

---
+++ IDS_Packet_Parser/models/event.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, asdict 
+from typing import Any 
+
+@dataclass 
+class IDSEvent:
+    packet_id: int 
+    timestamp: float 
+    
+    # network layer 
+    src_ip: str | None = None 
+    dst_ip: str | None = None 
+    network_proto: str | None = None 
+    
+    # transport layer 
+    src_port: int | None = None 
+    dst_port: int | None = None 
+    transport_proto: str | None = None 
+    transport_flags: str | None = None 
+    
+    # application layer 
+    app_proto: str = "UNKNOWN"
+    app_data: dict[str, Any] | None = None 
+    
+    # use dict in python for compatible with json format output 
+    def to_dict(self) -> dict[str, Any]:
+        return asdict(self)
