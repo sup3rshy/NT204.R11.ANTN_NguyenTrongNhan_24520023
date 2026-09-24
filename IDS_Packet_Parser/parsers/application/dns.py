925859ed80f379cc85ffe557df73afa94ed00f33
sup3rshy <133123465+sup3rshy@users.noreply.github.com>
2026-09-24 16:00:17 +0000

    Add DNS parser (tested)

--- IDS_Packet_Parser/parsers/application/dns.py
+++ IDS_Packet_Parser/parsers/application/dns.py
@@ -1,8 +1,35 @@
 from typing import Any
 from models.event import IDSEvent 
+from scapy.all import * 
+
 
 def parse(raw_packet: Any, event: IDSEvent) -> None: 
+    if not raw_packet.haslayer(DNS):
+        return 
     
+    dns = raw_packet[DNS]
     
+    try:
+        event.app_data = {}
+        
+        if dns.qr == 0:
+            event.app_data["type"] = "QUERY"
+        elif dns.qr == 1:
+            event.app_data["type"] = "RESPONSE"
+            
+        if dns.qdcount:
+            event.app_data["query_domain"] = dns.qd.qname.decode('utf-8', errors = "ignore").rstrip('.')
+            event.app_data["query_type"] = dns.qd.qtype 
+            
+        if dns.ancount:
+            event.app_data["response_name"] = dns.an[0].rrname.decode('utf-8', errors = "ignore").rstrip('.')
+            event.app_data["response_data"] = []
+            for i in range(dns.ancount):
+                rdata = dns.an[i].rdata 
+                if isinstance(rdata, bytes):
+                    rdata = rdata.decode('utf-8', errors = "ignore")
+                event.app_data["response_data"].append(rdata.rstrip('.'))
+        
+    except Exception as e:
+        event.app_data = {"error": str(e)}
     
-    pass
